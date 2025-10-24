8c423f2845e1c907da53b7d32da81d45b8433dc2
rcp82k <profpenteado.adm@gmail.com>
2025-10-23 21:26:21 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rola-dados-v1';
+const CACHE = 'rola-dados-v2';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
